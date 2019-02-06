814b04e2bee7b6dfb61673faa5cc5f96e3e6d7d8
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2019-02-06 20:02:42 +0530

    component add and include dynamic content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Components/Person/Person';
 
 class App extends Component {
+  constructor(){
+    super();
+    console.log("Hi am in App")
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code>I am Chanaka. I am Come Back React
+           I am Come back React
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Person />
+          <Person />
+          <Person />
+          <Person />
+
       </div>
     );
   }
