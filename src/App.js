231eefeb931faefbb0a206e2398ecdbff0db2c94
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2019-02-07 23:22:27 +0530

    handling events with method

--- src/App.js
+++ src/App.js
@@ -18,12 +18,20 @@ class App extends Component {
 
     ]
   }
+
+  switchNameHandler=()=>{
+    console.log("Clicked Switch Name");
+  }
+  
   render() {
     return (
       <div className="App">
           <p>
            I am Come back React
           </p>
+
+          <button onClick={this.switchNameHandler} >Switch Name</button>
+
           <Person name={this.state.person[0].name} age={this.state.person[0].age} >My hobby is Racing</Person>
           <Person name={this.state.person[1].name}  age={this.state.person[1].age} />
           <Person name={this.state.person[2].name}  age={this.state.person[2].age} />
